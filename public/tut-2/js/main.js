45ae931656312ddabecfb467e5cc21a4ec26caea
Erik Shuttlesworth <erik.shuttlesworth@gmail.com>
2017-08-14 15:37:30 -0500

    did nothing

---
+++ public/tut-2/js/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  let _game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+};
+
+// function preload() {
+
+//     game.load.image('background', 'images/background.png');
+
+// }
