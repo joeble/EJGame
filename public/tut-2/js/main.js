6c7557fccce24cf2334f71892d3f0357cbb89638
Joseph Knoebel <knoebelja@gmail.com>
2017-08-14 16:44:55 -0400

    asdkasldk

--- public/tut-2/js/main.js
+++ public/tut-2/js/main.js
@@ -1,9 +1,25 @@
-window.onload = function () {
-  let _game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+// =============================================================================
+// game states
+// =============================================================================
+
+PlayState = {};
+
+// load game assets here
+PlayState.preload = function () {
+    this.game.load.image('background', 'images/background.png');
 };
 
-// function preload() {
+// create game entities and set up world here
+PlayState.create = function () {
+    this.game.add.image(0, 0, 'background');
+};
 
-//     game.load.image('background', 'images/background.png');
+// =============================================================================
+// entry point
+// =============================================================================
 
-// }
+window.onload = function () {
+    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    game.state.add('play', PlayState);
+    game.state.start('play');
+};
